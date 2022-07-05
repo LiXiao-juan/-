8e085f3b35c1b747a38a193bd4b5f25e7c2addb4
Li <1627843848@qq.com>
2022-07-05 13:46:40 +0800

    项目初始化-调整初始化目录结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+// 引入全局样式
+import '@/styles/index.less'
+
+Vue.use(VueRouter)
+
+const routes = [
+]
+
+// 创建路由对象---传入规则
+const router = new VueRouter({
+  routes
+})
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
